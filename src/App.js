d0b8e8fc029d082feabf6473b4bb17d4d8a8e4aa
harshitt19 <harshit19tripathi@gmail.com>
2023-05-19 19:29:30 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [employees, setEmployees] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    // Fetch data from the provided endpoint
+    fetch('https://api.example.com/employees')
+      .then(response => response.json())
+      .then(data => setEmployees(data.employees))
+      .catch(error => console.error(error));
+  }, []);
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredEmployees = employees.filter((employee) =>
+    employee.id.toString().includes(searchTerm) ||
+    employee.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    employee.designation.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        placeholder="Search"
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+      {filteredEmployees.map((employee) => (
+        <div key={employee.id}>
+          <h2>{employee.name}</h2>
+          <p>ID: {employee.id}</p>
+          <p>Designation: {employee.designation}</p>
+          <ul>
+            {employee.skills.map((skill, index) => (
+              <li key={index}>{skill}</li>
+            ))}
+          </ul>
+          <h3>Projects:</h3>
+          {employee.projects && employee.projects.map((project, index) => (
+            <div key={index}>
+              <h4>{project.name}</h4>
+              <p>Description: {project.description}</p>
+              <ul>
+                {project.team.map((teamMember, index) => (
+                  <li key={index}>{teamMember.name} - {teamMember.role}</li>
+                ))}
+              </ul>
+              <h5>Tasks:</h5>
+              <ul>
+                {project.tasks.map((task, index) => (
+                  <li key={index}>
+                    {task.name} - Status: {task.status ? task.status : 'Not defined'}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
